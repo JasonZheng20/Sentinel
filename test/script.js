3fcd8b98ade72bb6048d35879b6ad3ce5fbbfd84
Jason Zheng <jzzheng@stanford.edu>
2017-06-24 15:33:01 -0700

    added test folder

---
+++ test/script.js
@@ -0,0 +1,5 @@
+const testElement = document.querySelector("#frame");
+console.log("frame Element: " + testElement);
+
+const testElementJs = testElement.querySelector("body");
+console.log(testElementJs);
