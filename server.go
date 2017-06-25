7b0e7b6f3db849646e6e7a92658f21098b3ac61e
rjeli <eli@rje.li>
2017-06-24 23:55:31 -0700

    ur a cuck

--- server.go
+++ server.go
@@ -5,26 +5,67 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/net/html"
+
+	"github.com/sfreiberg/gotwilio"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
+var twilio *gotwilio.Twilio
 
 func main() {
+	s := `
+<div>
+  A
+  <div>
+    B
+  </div>
+  <div>
+    C
+    <div>
+      D
+    </div>
+    E
+  </div>
+</div>
+  `
+	doc, e := html.Parse(strings.NewReader(s))
+	if e != nil {
+		panic(e)
+	}
+	log.Print(doc)
+	log.Print("[]: ", getContent(doc, []int{}))
+	log.Print("[0]: ", getContent(doc, []int{0}))
+	log.Print("[1]: ", getContent(doc, []int{1}))
+	log.Print("[0,0]: ", getContent(doc, []int{0, 0}))
+	log.Print("[0,1]: ", getContent(doc, []int{0, 1}))
+	log.Fatal("Done testing")
+
+	twilioSid := os.Getenv("TWILIO_SID")
+	twilioAuthToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	twilio = gotwilio.NewTwilioClient(twilioSid, twilioAuthToken)
 	var err error
 	db, err = sql.Open("sqlite3", "db.sqlite3")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	_, err = db.Exec("create table if not exists watch (" +
-		"url string," +
-		"node_address string," +
-		"phone_number string" +
+		"url string not null," +
+		"node_address text not null," +
+		"phone_number text not null," +
+		"content text" +
 		")")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	startWatcher()
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/watch", watchHandler)
 	http.ListenAndServe(":8080", nil)
@@ -65,7 +106,7 @@ func watchHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print(data)
 	nodeAddress, err := json.Marshal(data.NodeAddress)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	_, err = db.Exec("insert into watch (url,node_address,phone_number) "+
 		"values (?, ?, ?)", data.Url, nodeAddress, data.PhoneNumber)
@@ -75,4 +116,70 @@ func watchHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+}
+
+func startWatcher() {
+	ticker := time.NewTicker(time.Second * 5)
+	go func() {
+		for _ = range ticker.C {
+			log.Println("Checking pages")
+			var wg sync.WaitGroup
+			rows, err := db.Query("select url,node_address,phone_number,content " +
+				"from watch")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rows.Close()
+			for rows.Next() {
+				var url, nodeAddressString, phoneNumber, content string
+				err := rows.Scan(&url, &nodeAddressString, &phoneNumber, &content)
+				if err != nil {
+					log.Fatal(err)
+				}
+				var nodeAddress []int
+				err = json.Unmarshal([]byte(nodeAddressString), &nodeAddress)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					res, err := http.Get(url)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					defer res.Body.Close()
+					node, err := html.Parse(res.Body)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					newContent := getContent(node, nodeAddress)
+					if content == newContent {
+						log.Printf("content at %s has not changed\n", url)
+					} else {
+						log.Printf("content changed at %s!\n", url)
+					}
+				}()
+			}
+			wg.Wait()
+		}
+	}()
+}
+
+func getContent(node *html.Node, address []int) string {
+	if len(address) == 0 {
+		return node.Data
+	}
+	i := 0
+	for {
+		if address[0] == i {
+			return getContent(node.FirstChild, address[1:])
+		} else {
+			node = node.NextSibling
+			i++
+		}
+	}
 }
